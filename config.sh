ae27dab7812deeeb1f2fa5fa0da6dceca8cd9a83
Senpai2023 <136943928+Senpai2023@users.noreply.github.com>
2023-06-18 10:44:41 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,10 +1,10 @@
 # Base configuration (mandatory)
-var_jv_pg_fb_email="YOUR EMAIL ADDRESS HERE" # Facebook email used as username (used by Jarvis to send/receive messages/image)
-var_jv_pg_fb_password="YOUR PASSWORD HERE" # Facebook password
+var_jv_pg_fb_email="efrenjustin915@gmail.com" # Facebook email used as username (used by Jarvis to send/receive messages/image)
+var_jv_pg_fb_password="J1J2J3Justin" # Facebook password
 
 # Security configuration
 var_jv_pg_fb_allow_control_to_all="False" # Allow all people to control Jarvis (True/False)
-var_jv_pg_fb_allowed_people_ids="[\"id_1_here\", \"id_2_here\", \"id_3_here\", \"...\"]" # List of allowed people (facebook IDs) to control Jarvis
+var_jv_pg_fb_allowed_people_ids="[\"https://www.facebook.com/justin.efren.9?mibextid=LQQJ4d\", \"id_2_here\", \"id_3_here\", \"...\"]" # List of allowed people (facebook IDs) to control Jarvis
 
 # Used to send a messenger message from the command file or an other plugin (not needed for basic use of this plugin)
 var_jv_pg_fb_main_receiver_id="YOUR RECEIVER ID HERE" # User which will receive message from Jarvis (if configured in command file or in other plugin with this key)
